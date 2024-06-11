aabe4ac7e6b00f10e6a864262b836307cbfcc5dd
Rachel_Abit <rachelannabit@gmail.com>
2024-06-11 17:00:41 +0800

    new changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const book = document.querySelector('.book');
-const author = document.querySelector('.author');
-const pages = document.querySelector('.pages')
+const book_ = document.querySelector('.book');
+const author_ = document.querySelector('.author');
+const pages_ = document.querySelector('.pages')
 const button = document.querySelector('button');
 const contents = document.querySelector('.contents');
 
@@ -12,89 +12,75 @@ function Book(book, author, pages){
   this.pages = pages
 }
 
-// const book1 = new Book('Harry Potter', 'JK Rowling')
-// const book2 = new Book('Lord of the Rings',  'J.E Tolkien')
+const card = (book, author, pages) => {
+   const books = new Book (book, author, pages)
 
-const cards = (card) => {
-  let getbook = book.value;
-  let getAuthor = author.value;
-  let getPages = pages.value;
+   books.book = book_.value;
+   books.author = author_.value;
+   books.pages = pages_.value;
 
-  // const books = new Book(getbook, getAuthor, pages);
+   book_.value = ''
+   author_.value = ''
+   pages_.value = ''
+   
+    let cards = document.createElement('div');
+    let book_title = document.createElement('h3');
+    let author_name = document.createElement('p');
+    let pages_num = document.createElement('p');
+    let remove_btn = document.createElement('button');
+    let checkbox = document.createElement('input')
+    let checkbox_container = document.createElement('div');
+    let check_label = document.createElement('label');
 
-  book.value = '';
-  author.value = '';
-  pages.value = '';
+    cards.style.width = '250px';
+    cards.style.height = '250px';
+    cards.style.marginTop = '20px';
+    cards.style.padding = '10px';
+    cards.style.display = 'flex'
+    cards.style.flexDirection = 'column';
+    cards.style.boxShadow = '0px 5px 10px rgba(0,0,0,0.15)'
+    cards.style.borderRadius = '10px';
 
-   if(card > 10){
-    alert('You reach maximum of 10');
-   } else {
-      for (let i = 0; i < 1; i++){
+    book_title.style.fontSize = '15px';
+    author_name.style.fontSize = '15px';
+    pages_num.style.fontSize = '15px';
 
-        let tiny_cards = document.createElement('div');
-        let book_title = document.createElement('h3');
-        let author_name = document.createElement('p');
-        let pages_num = document.createElement('p');
-        let remove_btn = document.createElement('button');
-        let checkbox = document.createElement('input')
-        let checkbox_container = document.createElement('div');
-        let check_label = document.createElement('label');
-        tiny_cards.style.marginTop = '20px';
-        tiny_cards.style.padding = '10px'
-        tiny_cards.style.display = 'flex';
-        tiny_cards.style.flexDirection = 'column';
-        tiny_cards.style.gap = '10px'
-        tiny_cards.style.width = '250px';
-        tiny_cards.style.height = '250px';
-        tiny_cards.style.boxShadow = '0px 5px 10px rgba(0,0,0,0.15)'
-        tiny_cards.style.borderRadius = '10px';
-        tiny_cards.style.backgroundColor = 'white';
-        book_title.style.fontSize = '17px';
-        author_name.style.fontSize = '15px';
-        pages_num.style.fontSize = '15px';
-        remove_btn.innerText = 'Remove';
-        remove_btn.style.marginTop = '54px';
-        remove_btn.style.padding = '10px';
-        remove_btn.style.backgroundColor = 'orangered';
-        remove_btn.style.border = 'none';
-        remove_btn.style.color = 'White'
-        remove_btn.style.fontWeight = 'bold';
-        remove_btn.style.cursor = 'pointer'
-        checkbox.type = 'checkbox';
-        checkbox_container.style.width = '100%';
-        checkbox_container.style.display = 'flex';
-        checkbox_container.style.alignItems = 'center';
-        checkbox_container.style.gap = '10px';
-        check_label.innerText = 'Read';
-        
-        contents.style.display = 'flex';
+    remove_btn.innerText = 'Remove';
+    remove_btn.style.marginTop = '90px';
+    remove_btn.style.padding = '10px';
+    remove_btn.style.backgroundColor = 'orangered';
+    remove_btn.style.border = 'none';
+    remove_btn.style.color = 'White'
+    remove_btn.style.fontWeight = 'bold';
+    remove_btn.style.cursor = 'pointer';
 
-        book_title.textContent = `Book title: ${getbook}`;
-        author_name.textContent = `Author name: ${getAuthor}`;
-        pages_num.textContent = `Pages: ${getPages}`;
+    checkbox.type = 'checkbox';
+    checkbox_container.style.width = '100%';
+    checkbox_container.style.display = 'flex';
+    checkbox_container.style.alignItems = 'center';
+    checkbox_container.style.gap = '10px';
+    check_label.innerText = 'Read';
 
-        remove_btn.onclick = remove();
+    contents.style.display = 'flex';
 
-        tiny_cards.append(book_title);
-        tiny_cards.append(author_name);
-        tiny_cards.append(pages_num);
-        tiny_cards.append(checkbox_container);
-        tiny_cards.append(remove_btn)
-        checkbox_container.append(checkbox);
-        checkbox_container.append(check_label);
-        
-        contents.append(tiny_cards);
-        library.push(tiny_cards)
+    book_title.textContent = `Book Title: ${books.book}`;
+    author_name.textContent = `Author name: ${books.author}`;
+    pages_num.textContent = `Pages: ${books.pages}`;
 
-        console.log(library)
-      }
-   }
+    cards.append(book_title);
+    cards.append(author_name);
+    cards.append(pages_num);
+    cards.append(checkbox_container);
+    cards.append(remove_btn)
+    checkbox_container.append(checkbox);
+    checkbox_container.append(check_label);
+
+    contents.append(cards);
+    library.push(books);
+    console.log(library)
+   
 }
 
 function addToBook(){
-   cards();
-}
-
-function remove(){
-  
+  card();
 }
