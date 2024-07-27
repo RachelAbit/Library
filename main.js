b3e3c06e568a8bcd3a366537447a23b10f7a7ce9
Rachel_Abit <rachelannabit@gmail.com>
2024-07-27 20:56:59 +0800

    Not yet Finished

--- main.js
+++ main.js
@@ -1,95 +1,45 @@
-const book_ = document.querySelector('.book');
-const author_ = document.querySelector('.author');
-const pages_ = document.querySelector('.pages')
-const button = document.querySelector('button');
-const contents = document.querySelector('.contents');
+class Library {
+  constructor(author_name, bookTitle, bookPages){
+    this.author_name = author_name;
+    this.bookTitle = bookTitle;
+    this.bookPages = bookPages;
+  }
 
-const library = [] // I need to push all the card on the library array
-
-function Book(book, author, pages){
-  this.book = book
-  this.author = author
-  this.pages = pages
+  displayCard(){
+   const main = document.querySelector('.contents');
+   const card = document.createElement('div');
+   const btn_div = document.createElement('div');
+   card.setAttribute('class', 'cards');
+   card.innerHTML = `
+    <h1>Book Title: <span>${this.bookTitle}</span></h1>
+    <h2>Author: <span>${this.author_name}</span></h2>
+    <h3>Pages: <span>${this.bookPages}</span></h3>
+   `;
+   btn_div.innerHTML = `
+    <input type="checkbox"> &nbsp;Read
+    <button> Delete </button>
+   `
+   card.appendChild(btn_div);
+   main.appendChild(card);
+    
+  }
 }
 
-const card = (book, author, pages) => {
-   const books = new Book (book, author, pages)
+const library = []; //to store the cards
 
-   books.book = book_.value;
-   books.author = author_.value;
-   books.pages = pages_.value;
+const btn = document.getElementById('btn');
 
-   book_.value = ''
-   author_.value = ''
-   pages_.value = ''
-   
-    let cards = document.createElement('div');
-    cards.id = 'cards_';
-    let book_title = document.createElement('h3');
-    let author_name = document.createElement('p');
-    let pages_num = document.createElement('p');
-    let remove_btn = document.createElement('button');
-    let checkbox = document.createElement('input')
-    let checkbox_container = document.createElement('div');
-    let check_label = document.createElement('label');
+btn.addEventListener('click', ()=>{
+    
+     let getAuthr = document.getElementById('author_name').value;
+     let getBookT = document.getElementById('book_title').value;
+     let getBookP = document.getElementById('book_pages').value;
 
-    cards.style.width = '250px';
-    cards.style.height = '250px';
-    cards.style.marginTop = '20px';
-    cards.style.padding = '10px';
-    cards.style.display = 'flex'
-    cards.style.flexDirection = 'column';
-    cards.style.boxShadow = '0px 5px 10px rgba(0,0,0,0.15)'
-    cards.style.borderRadius = '10px';
+     const newLibrary = new Library(getAuthr, getBookT, getBookP);
 
-    book_title.style.fontSize = '15px';
-    author_name.style.fontSize = '15px';
-    pages_num.style.fontSize = '15px';
+     library.push(newLibrary);
+     newLibrary.displayCard();
+     
+     console.log(newLibrary);
 
-    remove_btn.innerText = 'Remove';
-    remove_btn.style.marginTop = '90px';
-    remove_btn.style.padding = '10px';
-    remove_btn.style.backgroundColor = 'orangered';
-    remove_btn.style.border = 'none';
-    remove_btn.style.color = 'White'
-    remove_btn.style.fontWeight = 'bold';
-    remove_btn.style.cursor = 'pointer';
-
-    checkbox.type = 'checkbox';
-    checkbox_container.style.width = '100%';
-    checkbox_container.style.display = 'flex';
-    checkbox_container.style.alignItems = 'center';
-    checkbox_container.style.gap = '10px';
-    check_label.innerText = 'Read';
-
-    contents.style.display = 'flex';
-
-    book_title.textContent = `Book Title: ${books.book}`;
-    author_name.textContent = `Author name: ${books.author}`;
-    pages_num.textContent = `Pages: ${books.pages}`;
-
-    cards.append(book_title);
-    cards.append(author_name);
-    cards.append(pages_num);
-    cards.append(checkbox_container);
-    cards.append(remove_btn)
-    checkbox_container.append(checkbox);
-    checkbox_container.append(check_label);
-
-    contents.append(cards);
-    library.push(books);
-    console.log(library)
-
-    remove_btn.addEventListener('click', () => {
-        let remove_cards = document.getElementById('cards_');
-        if(remove_cards){
-          remove_cards.remove();
-          library.pop();
-        }
-    })
-}
-
-function addToBook(){
-  card();
-}
-
+});
